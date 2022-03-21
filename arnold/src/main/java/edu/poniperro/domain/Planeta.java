dbc56735b47c193cd599afd8fd7d2c925de7e11e
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-03-21 23:04:12 +0100

    feat: add enum constants

---
+++ arnold/src/main/java/edu/poniperro/domain/Planeta.java
@@ -0,0 +1,16 @@
+package edu.poniperro.domain;
+
+/**
+ * An enum (also known as enumeration and enumerated data type)
+ * lets you create an ordered list of constants as a type.
+ */
+public enum Planeta {
+    MERCURY (3.303e+23, 2.4397e6),
+    VENUS (4.869e+24, 6.0518e6),
+    EARTH (5.976e+24, 6.37814e6),
+    MARS (6.421e+23, 3.3972e6),
+    JUPITER (1.9e+27, 7.1492e7),
+    SATURN (5.688e+26, 6.0268e7),
+    URANUS (8.686e+25, 2.5559e7),
+    NEPTUNE (1.024e+26, 2.4746e7);
+}
